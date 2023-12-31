63825f4eef7da4a661a518330db1146bb241e013
41223242 <144992436+41223242@users.noreply.github.com>
2023-12-31 10:31:49 +0000

    新增ANSIC與內容

---
+++ a1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // 印出姓名
+    printf("姓名   : jiean Yeh\n"); 
+
+    // 印出出生日期
+    printf("生日   : 2005年1月31日\n"); 
+
+    // 印出手機號碼
+    printf("手機   : 66-666666\n"); 
+
+    // 表示程式執行成功
+    return 0; 
+}
